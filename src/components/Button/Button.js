7a304037101d062746112d0e5c5d058355cba37d
Olha Kushyk <olha.kushyk@gmail.com>
2023-03-06 12:01:31 +0200

    Add Redux

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,6 @@
 import styles from './Button.module.scss';
 
-const Button = props => {
-    return (<button className={styles.button}>{props.children}</button>);
-};
+const Button = props => <button className={styles.button}>{props.children}</button>;
 
 export default Button;
 

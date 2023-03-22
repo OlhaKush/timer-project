fe6a6b483346f424172e07c9ee5e3e56bcfcff2e
Olha Kushyk <olha.kushyk@gmail.com>
2023-03-22 14:33:41 +0200

    Add favorite

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,50 @@
 import styles from './Card.module.scss';
+import Button from "../Button/Button";
+import {useDispatch} from 'react-redux';
+import { toggleCard } from '../../redux/store';
+import clsx from 'clsx';
 
 const Card = props => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(toggleCard(props.id));
+        };
+
     return (
-        <li className={styles.card}>{props.title}</li>
+        <li className={styles.card}>{props.title}
+            <Button >
+                <span className={clsx("fa fa-star-o", props.isFavorite ? styles.isFavorite : styles.notFavorite)} onClick={handleSubmit}/>
+            </Button>
+            <Button>
+                <span className="fa fa-trash"/>
+            </Button>
+        </li>
     );
 };
 
+
+// const Card = props => {
+//     const [isFavorite, setFavorite] = useState('');
+//
+//     const toggleClass = () => {
+//         setFavorite(!isFavorite);
+//     };
+//
+//     return (
+//         <li className={styles.card}>{props.title}
+//             <Button >
+//                 <span className={clsx("fa fa-star-o", isFavorite ? styles.isFavorite : styles.notFavorite)} onClick={toggleClass}/>
+//             </Button>
+//             <Button>
+//                 <span className="fa fa-trash"/>
+//             </Button>
+//         </li>
+//     );
+// };
+
 export default Card;
+
+
+
--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,13 +1,24 @@
 import styles from './Favorite.module.scss';
 import Container from "../Container/Container";
 import PageTitle from "../PageTitle/PageTitle";
+import Card from "../Card/Card";
+import { useSelector } from 'react-redux';
+import { getFavorite } from '../../redux/store';
 
-const Favorite = () => {
+
+const Favorite = props => {
+
+    const cards = useSelector(state => getFavorite(state, props.id));
 
     return (
         <Container >
             <PageTitle>Favorite</PageTitle>
             <p>Some text here</p>
+            <article className={styles.column}>
+                <ul className={styles.cards}>
+                    {cards.map(card => <Card key={card.id} id={card.id} title={card.title} isFavorite={card.isFavorite}/>)}
+                </ul>
+            </article>
         </Container>
     );
 };
